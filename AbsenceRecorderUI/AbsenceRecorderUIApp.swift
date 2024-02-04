f6a7ccbe510b477f1a342ee1e0e5dc8998185d35
Koza Kurumlu <kozakurumlu@gmail.com>
2024-02-04 11:56:12 +0000

    Initial Commit

---
+++ AbsenceRecorderUI/AbsenceRecorderUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderUIApp.swift
+//  AbsenceRecorderUI
+//
+//  Created by Kurumlu, Koza (RCH) on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
