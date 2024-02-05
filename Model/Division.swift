78e2c9821ce2bec5c444086c532da4df01436e49
Koza Kurumlu <kozakurumlu@gmail.com>
2024-02-05 20:12:36 +0000

    Spelt examples correctly :)

--- Model/Division.swift
+++ Model/Division.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class Division{
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String){
+        self.code = code
+    }
+    
+    #if DEBUG
+    static func createDivision(code: String, of size: Int) -> Division{
+        let division = Division(code: code)
+        for _ in 0..<size{
+            let new_student = Student(forename: "Forename", surname: "Surname", birthday: .now)
+            division.students.append(new_student)
+        }
+        return division
+    }
+    
+    static let examples = [Division.createDivision(code: "vBY-1", of: 8),
+                           Division.createDivision(code: "vCY-1", of: 12),
+                           Division.createDivision(code: "vCZ-1", of: 9),
+                           Division.createDivision(code: "xD1-1", of: 10),
+                           Division.createDivision(code: "vBX-2", of: 7)]
+    #endif
+}
--- View/ContentView.swift
+++ View/ContentView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    var divisions: [Division]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Text(Division.examples[0].code)
         }
         .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View{
+        ContentView(divisions: Division.examples)
+    }
 }
