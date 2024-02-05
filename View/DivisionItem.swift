e02c20b5be0d314767c8a83141b1edb94b7d435c
Koza Kurumlu <kozakurumlu@gmail.com>
2024-02-05 20:57:56 +0000

    DivisionItem added.

---
+++ View/DivisionItem.swift
@@ -0,0 +1,18 @@
+//
+//  DivisionItem.swift
+//  AbsenceRecorderUI
+//
+//  Created by Kurumlu, Koza (RCH) on 05/02/2024.
+//
+
+import SwiftUI
+
+struct DivisionItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DivisionItem()
+}
